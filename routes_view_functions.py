56d7e6f930006ca3beabede29cd36b9c28eaec22
phil-baller <philballer41@gmail.com>
2023-09-11 10:31:37 +0100

    Rendering templates using Jinja2 syntax

--- routes_view_functions.py
+++ routes_view_functions.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template, url_for, request
+from flask import make_response, redirect
 
 app = Flask(__name__)
 
-@app.route('/<name>')
-def user(name):
-    return '<h1> Your called %s</h1>' %name
+@app.route('/')
+def index():
+    return redirect('https://www.google.com')
 
-user('Philemon Tebo')
+
+@app.route('/<user>')
+def new_user(user):
+    return '<h2> Welcome %s' %user
+
+new_user('baller')
+
+if __name__ == '__main__':
+    app.run(debug=True)
