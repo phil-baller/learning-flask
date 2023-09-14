f9fa657dca66f7e77809e891ab476cfabda30168
phil-baller <philballer41@gmail.com>
2023-09-14 02:52:23 +0100

    Returning server errors and routing urls

--- routes_view_functions.py
+++ routes_view_functions.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for
+from flask import make_response
 from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -10,6 +12,14 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
