ae9e2cadbb6cd58c7797ad7dd2320c1ae8b2632d
phil-baller <philballer41@gmail.com>
2023-09-14 08:44:57 +0100

    fixed datetime issue (library was not imported)

--- routes_view_functions.py
+++ routes_view_functions.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, url_for
-from flask import make_response
 from flask_bootstrap import Bootstrap
-
+from datetime import datetime
+from flask_moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
