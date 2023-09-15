81cc760f185a0c7f85f6c66c7722c2143d8dc0ac
phil-baller <philballer41@gmail.com>
2023-09-15 23:26:25 +0100

    working with database files and creating tables

--- routes_view_functions.py
+++ routes_view_functions.py
@@ -2,14 +2,40 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, form
+from wtforms import StringField, SubmitField
 from wtforms.validators import data_required
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = "This text is going to be used to secure the platform"
+app.app_context().push()
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref="role")
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[data_required()])
@@ -21,7 +47,7 @@ def index():
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+            flash('Looks like you have changed your name to %s!' % form.name.data)
             session['name'] = form.name.data
             form.name.data = ''
             return redirect(url_for('index'))
